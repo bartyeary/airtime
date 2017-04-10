aa6bb4a58d183c9fe8397def059d0ca4545633bb
bartyeary <bart.yeary@gmail.com>
2017-04-10 14:02:09 -0700

    interactive bot

--- loading/js/main.js
+++ loading/js/main.js
@@ -27,13 +27,26 @@ function returnreflection(){
 	TweenMax.to(reflection, 1, {top:20, ease:Back.easeInOut});
 }
 
+var botSpin = document.querySelector("#botSpin");
+var botTalk = document.querySelector("#botTalk");
+var bot = document.querySelector("#bot");
+var pulseDark = document.querySelector("#pulseDark");
+var pulseLight = document.querySelector("#pulseLight");
 
 var spin = TweenMax.from(botSpin, 0.8, {backgroundPosition:"1840px", paused:true, ease:SteppedEase.config(23)});
 
-var showtalk = TweenMax.from(botTalk, 0.1, {opacity:0, paused:true, delay:0.6});
+var showtalk = TweenMax.from(botTalk, 0, {opacity:0, paused:true, delay:0.7});
 var starttalk = TweenMax.from(botTalk, 0.8, {repeat:2, backgroundPosition:"1520px", paused:true, ease:SteppedEase.config(19), delay:.7});
-var hidetalk = TweenMax.to(botTalk, 0.1, {opacity:0, paused:true, delay:3.2});
-var showbot = TweenMax.from(bot, 0.1, {opacity:0, paused:true, delay:3.1});
+var hidetalk = TweenMax.to(botTalk, 0, {opacity:0, paused:true, delay:3.2});
+
+var showbot = TweenMax.from(bot, 0, {opacity:0, paused:true, delay:3.2});
+var hidebot = TweenMax.to(bot, 2, {opacity:0, paused:true, delay:5});
+
+var showpulsedark = TweenMax.from(pulseDark, 2, {opacity:0, paused:true, delay:2});
+var hidepulsedark = TweenMax.to(pulseDark, 2, {opacity:0, paused:true, delay:5});
+
+var showpulselight = TweenMax.from(pulseLight, 2, {opacity:0, paused:true, delay:2});
+var hidepulselight = TweenMax.to(pulseLight, 2, {opacity:0, paused:true, delay:5});
 
 
 botbutt.onmousedown = function(){
@@ -42,18 +55,25 @@ botbutt.onmousedown = function(){
 	showtalk.restart(true);
 	hidetalk.restart(true);
 	showbot.restart(true);
+	hidebot.restart(true);
+	showpulsedark.restart(true);
+	showpulselight.restart(true);
+	hidepulsedark.restart(true);
+	hidepulselight.restart(true);
 }
 
 
-// var botSpin = document.querySelector("#botSpin");
-// var botTalk = document.querySelector("#botTalk");
-// var bot = document.querySelector("#bot");
-// 
+
+
 // var tl = new TimelineMax();
+// 
 // tl.from(botSpin, 0.8, {backgroundPosition:"1840px", paused:true, ease:SteppedEase.config(23)});	
-// tl.from(botTalk, 0, {opacity:0});
+// tl.to(botSpin, 0.8, {backgroundPosition:"-1840px", paused:true, ease:SteppedEase.config(23)});	
+// 
+// tl.to(botTalk, 0, {opacity:1});
 // tl.from(botTalk, 0.8, {repeat:2, backgroundPosition:"1520px", paused:true, ease:SteppedEase.config(19), delay:0.8});
 // 
 // 
-// tl.play();
-
+// botbutt.onmousedown = function(){
+// 	tl.play();
+// }
